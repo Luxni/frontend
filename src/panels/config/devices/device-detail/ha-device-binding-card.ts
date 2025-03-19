896871bb86c6a2d7b7520ff5110a06e07429d753
luxni <307993459@qq.com>
2025-03-20 00:35:36 +0800

    feat: add binding table add/delete button.

--- src/panels/config/devices/device-detail/ha-device-binding-card.ts
+++ src/panels/config/devices/device-detail/ha-device-binding-card.ts
@@ -25,8 +25,8 @@ export class HaDeviceBindingCard extends LitElement {
   @state()
   public showHidden = false;
 
-  @state()
-  private bindings!: MatterNodeBinding[];
+  @property({ attribute: false })
+  public bindings?: MatterNodeBinding[];
 
   private _handleNodeBindingChanged(event: CustomEvent<{ nodeBinding: [] }>) {
     if (event.detail.nodeBinding) {
@@ -63,26 +63,28 @@ export class HaDeviceBindingCard extends LitElement {
               <div id="entities" class="move-up">
                 <mwc-list>
                   ${bindings.map(
-                    (device) => html`
-                      <div>
-                        <mwc-list-item>
-                          <div
-                            style="display: flex; align-items: center; width: 100%;"
+                    (device, index) => html`
+                      <mwc-list-item tabindex="0" data-index=${index}>
+                        <div
+                          style="display: flex; align-items: center; width: 100%;"
+                        >
+                          <ha-svg-icon>${index}</ha-svg-icon>
+                          <span style="flex-grow: 1; text-align: center;">
+                            ${device.node_id + "/" + device.endpoint_id}
+                          </span>
+                          <mwc-button
+                            slot="end"
+                            class="action-button"
+                            data-index=${index}
+                            style="margin-left: auto;"
+                            @click=${this._bindingsDelete}
                           >
-                            <ha-svg-icon
-                              path="M5.12,5H18.87L17.93,4H5.93L5.12,5M20.54,5.23C20.83,5.57 21,6 21,6.5V19A2,2 0 0,1 19,21H5A2,2 0 0,1 3,19V6.5C3,6 3.17,5.57 3.46,5.23L4.84,3.55C5.12,3.21 5.53,3 6,3H18C18.47,3 18.88,3.21 19.15,3.55L20.54,5.23M6,18H12V15H6V18Z"
-                            ></ha-svg-icon>
-                            <span style="flex-grow: 1; text-align: center;">
-                              ${device.node_id + "/" + device.endpoint_id}
-                            </span>
-                            <mwc-button style="margin-left: auto;">
-                              ${this.hass.localize(
-                                "ui.panel.config.devices.entities.binding.delete"
-                              )}
-                            </mwc-button>
-                          </div>
-                        </mwc-list-item>
-                      </div>
+                            ${this.hass.localize(
+                              "ui.panel.config.devices.entities.binding.delete"
+                            )}
+                          </mwc-button>
+                        </div>
+                      </mwc-list-item>
                     `
                   )}
                 </mwc-list>
@@ -102,11 +104,15 @@ export class HaDeviceBindingCard extends LitElement {
   }
 
   private _bindingsAdd(_ev: Event): void {
-    // console.log(ev);
+    const nodeBinding: MatterNodeBinding = { node_id: 1, endpoint_id: 2 };
+    this.bindings?.push(nodeBinding);
+    this.requestUpdate();
   }
 
   private _bindingsDelete(_ev: Event): void {
-    // console.log(ev);
+    const button = _ev.currentTarget as HTMLElement;
+    this.bindings?.splice(button.dataset.index, 1);
+    this.requestUpdate();
   }
 
   static styles = css`
